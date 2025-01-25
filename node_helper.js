20f86206407b4a3f5143591b6de6a5818ee45fa8
2hdlockness <77622028+2hdlockness@users.noreply.github.com>
2025-01-25 22:39:41 +0100

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -1,12 +1,63 @@
-const NodeHelper = require("node_helper")
+const NodeHelper = require("node_helper");
+const axios = require("axios");
+const schedule = require("node-schedule");
 
 module.exports = NodeHelper.create({
-
-  async socketNotificationReceived(notification, payload) {
-    if (notification === "GET_RANDOM_TEXT") {
-      const amountCharacters = payload.amountCharacters || 10
-      const randomText = Array.from({ length: amountCharacters }, () => String.fromCharCode(Math.floor(Math.random() * 26) + 97)).join("")
-      this.sendSocketNotification("EXAMPLE_NOTIFICATION", { text: randomText })
-    }
+  
+  start: function () {
+    console.log("MMM-Linky node_helper démarré...");
+    this.scheduleDataFetch();
   },
-})
+
+scheduleDataFetch: function () {  
+    const randomHour = Math.floor(Math.random() * (10 - 6 + 1)) + 6;
+    const randomMinute = Math.floor(Math.random() * 60); 
+    const cronExpression = `${randomMinute} ${randomHour} * * *`;
+    const job = schedule.scheduleJob(cronExpression, () => {  
+        console.log(`Exécution de la récupération des données à ${randomHour}:${randomMinute < 10 ? '0' : ''}${randomMinute}`);  
+        this.sendnotificationschedule();
+    });
+
+    console.log(`Tâche planifiée pour ${randomHour}:${randomMinute < 10 ? '0' : ''}${randomMinute} UTC.`);  
+},
+
+sendnotificationschedule : function(){
+	this.sendSocketNotification("CONSUMPTION_SCHEDULE");
+},
+
+
+getConsumptionData: function (url, token, prm, startDate, endDate, period) {
+    const endpoint = `${url}?prm=${prm}&start=${startDate}&end=${endDate}`;
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    };
+
+    console.log(`Requête API envoyée : ${endpoint}`);
+
+    axios
+      .get(endpoint, config)
+
+      .then((response) => {
+		console.log("Données reçues de l'API :", response.data);
+        const data = response.data;
+
+        this.sendSocketNotification("CONSUMPTION_DATA", data);
+      })
+
+      .catch((error) => {
+        console.error("Erreur lors de la récupération des données :", error.response ? error.response.data : error.message);
+      });
+  },
+  
+socketNotificationReceived: function (notification, payload) {
+
+  if (notification === "GET_CONSUMPTION_DATA") {
+    console.log("Notification GET_CONSUMPTION_DATA reçue, récupération des données...");
+    const { url, token, prm, startDate, endDate, period} = payload;
+    this.getConsumptionData(url, token, prm, startDate, endDate, period);
+  }
+},
+});
