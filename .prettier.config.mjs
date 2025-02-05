3fc737ad909689ff9373ff1f3466f5bdb185ff79
bugsounet <github@bugsounet.fr>
2025-02-05 06:54:25 +0100

    add test suite

---
+++ .prettier.config.mjs
@@ -0,0 +1,14 @@
+const config = {
+  tabWidth: 2,
+  overrides: [
+    {
+      files: "*.md",
+      options: {
+        parser: "markdown"
+      }
+    }
+  ],
+  trailingComma: "none"
+};
+
+export default config;
