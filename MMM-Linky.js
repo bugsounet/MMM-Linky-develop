db712707672e7eaf3d03763436cca31f1819a93e
bugsounet <github@bugsounet.fr>
2025-02-07 01:17:16 +0100

    first review

--- MMM-Linky.js
+++ MMM-Linky.js
@@ -2,9 +2,9 @@
 
 Module.register("MMM-Linky", {
   defaults: {
-    url: "https://conso.boris.sh/api/daily_consumption",
-    token: "votre-token-ici",
-    prm: "123456789",
+    debug: false,
+    token: "",
+    prm: "",
     periode: 1,
     annee_n_minus_1: 1,
     couleur: 3,
@@ -17,15 +17,18 @@ Module.register("MMM-Linky", {
     this.consumptionData = {};
     this.chart = null;
     this.ChartJsLoaded = false;
-    this.getData();
-
-    setTimeout(() => {
-      this.updateDom();
-    }, 50000);
+    this.data.header = this.getHeaderText();
   },
 
   getStyles () {
     return ["MMM-Linky.css"];
+  },
+
+  getScripts () {
+    return [
+      this.file("node_modules/chart.js/dist/chart.umd.js"),
+      this.file("node_modules/chartjs-plugin-datalabels/dist/chartjs-plugin-datalabels.min.js")
+    ];
   },
 
   getChartColors () {
@@ -48,41 +51,8 @@ Module.register("MMM-Linky", {
     return periodTexts[this.config.periode] || "Consommation électricité";
   },
 
-  getData () {
-    const dates = this.calculateDates();
-    if (!dates) return;
-
-    this.sendSocketNotification("GET_CONSUMPTION_DATA", {
-      url: this.config.url,
-      token: this.config.token,
-      prm: this.config.prm,
-      startDate: dates.startDate,
-      endDate: dates.endDate,
-      period: this.config.periode
-    });
-
-    if (this.config.annee_n_minus_1 === 1) {
-      const previousYearDates = this.calculateDates(1);
-      if (previousYearDates) {
-        this.sendSocketNotification("GET_CONSUMPTION_DATA", {
-          url: this.config.url,
-          token: this.config.token,
-          prm: this.config.prm,
-          startDate: previousYearDates.startDate,
-          endDate: previousYearDates.endDate,
-          period: this.config.periode
-        });
-      }
-    }
-  },
-
   getDom () {
     let wrapper = document.createElement("div");
-
-    const header = document.createElement("div");
-    header.innerHTML = this.getHeaderText();
-    header.classList.add("header");
-    wrapper.appendChild(header);
 
     if (Object.keys(this.consumptionData).length > 0) {
       console.log("Données de consommation trouvées, préparation du graphique...", this.consumptionData);
@@ -105,7 +75,7 @@ Module.register("MMM-Linky", {
           days.push(
             ...data.map(
               (item) => `${item.day}-${
-                ["janv", "févr", "mars", "avr", "mai", "juin", "juil", "août", "sept", "oct", "nov", "déc"][item.month]
+                ["Error", "janv", "févr", "mars", "avr", "mai", "juin", "juil", "août", "sept", "oct", "nov", "déc"][item.month]
               }`
             )
           );
@@ -126,175 +96,99 @@ Module.register("MMM-Linky", {
       let chartContainer = document.createElement("canvas");
       wrapper.appendChild(chartContainer);
 
+      try {
+        this.createChart(chartContainer, days, datasets);
+        console.log("Graphique créé avec succès");
+
+        // todo: A afficher APRES Le dom créé
+        if (chartContainer.width > 0 && chartContainer.height > 0) {
+
+          const currentYearTotal = this.calculateTotalConsumption(new Date().getFullYear().toString());
+          const previousYearTotal = this.calculateTotalConsumption((new Date().getFullYear() - 1).toString());
+
+          let message = "";
+          let color = "";
+          let periodText = "";
+
+          switch (this.config.periode) {
+            case 1:
+              periodText = "le dernier jour";
+              break;
+            case 2:
+              periodText = "les 3 derniers jours";
+              break;
+            case 3:
+              periodText = "les 7 derniers jours";
+              break;
+            default:
+              periodText = "période inconnue";
+          }
+
+          if (currentYearTotal < previousYearTotal) {
+            message = `Félicitations, votre consommation d'énergie a baissé sur ${periodText} par rapport à l'année dernière !`;
+            color = "green";
+          } else if (currentYearTotal > previousYearTotal) {
+            message = `Attention, votre consommation d'énergie a augmenté sur ${periodText} par rapport à l'année dernière !`;
+            color = "red";
+          } else {
+            message = `Votre consommation d'énergie est stable sur ${periodText} par rapport à l'année dernière.`;
+            color = "yellow";
+          }
+
+          // todo: a revoir (mettre ID et utiliser CSS)
+          const messageElement = document.createElement("div");
+          messageElement.innerHTML = `<span style="color: ${color};">${message}</span>`;
+          messageElement.style.textAlign = "left";
+          messageElement.style.fontSize = "16px";
+          messageElement.style.maxWidth = "100%";
+          messageElement.style.wordWrap = "break-word";
+          messageElement.style.lineHeight = "1.2";
+          messageElement.style.marginTop = "10px";
+          wrapper.appendChild(messageElement);
+        } else {
+          console.log("Le graphique n'a pas été affiché correctement, tentative de réinitialisation.");
+        }
+
+      } catch (error) {
+        console.error("Erreur lors de la création du graphique : ", error);
+      }
+
+    } else {
+      // todo: a revoir (id/css)
       let waitingMessage = document.createElement("div");
-      waitingMessage.innerHTML = "Veuillez patienter, vos données arrivent...";
+      waitingMessage.textContent = "Veuillez patienter, vos données arrivent...";
       waitingMessage.style.color = "#ffffff";
       waitingMessage.style.textAlign = "center";
       waitingMessage.style.marginTop = "10px";
       wrapper.appendChild(waitingMessage);
-
-      this.ensureChartJsLoaded(() => {
-        try {
-          this.createChart(chartContainer, days, datasets);
-          console.log("Graphique créé avec succès");
-
-          wrapper.removeChild(waitingMessage);
-
-          setTimeout(() => {
-            if (chartContainer.width > 0 && chartContainer.height > 0) {
-
-              const currentYearTotal = this.calculateTotalConsumption(new Date().getFullYear().toString());
-              const previousYearTotal = this.calculateTotalConsumption((new Date().getFullYear() - 1).toString());
-
-              let message = "";
-              let color = "";
-              let periodText = "";
-
-              switch (this.config.periode) {
-                case 1:
-                  periodText = "le dernier jour";
-                  break;
-                case 2:
-                  periodText = "les 3 derniers jours";
-                  break;
-                case 3:
-                  periodText = "les 7 derniers jours";
-                  break;
-                default:
-                  periodText = "période inconnue";
-              }
-
-              if (currentYearTotal < previousYearTotal) {
-                message = `Félicitations, votre consommation d'énergie a baissé sur ${periodText} par rapport à l'année dernière !`;
-                color = "green";
-              } else if (currentYearTotal > previousYearTotal) {
-                message = `Attention, votre consommation d'énergie a augmenté sur ${periodText} par rapport à l'année dernière !`;
-                color = "red";
-              } else {
-                message = `Votre consommation d'énergie est stable sur ${periodText} par rapport à l'année dernière.`;
-                color = "yellow";
-              }
-
-              const messageElement = document.createElement("div");
-              messageElement.innerHTML = `<span style="color: ${color};">${message}</span>`;
-              messageElement.style.textAlign = "left";
-              messageElement.style.fontSize = "16px";
-              messageElement.style.maxWidth = "100%";
-              messageElement.style.wordWrap = "break-word";
-              messageElement.style.lineHeight = "1.2";
-              messageElement.style.marginTop = "10px";
-              wrapper.appendChild(messageElement);
-            } else {
-              console.log("Le graphique n'a pas été affiché correctement, tentative de réinitialisation.");
-            }
-          }, 1000);
-
-        } catch (error) {
-          console.error("Erreur lors de la création du graphique : ", error);
-        }
-      });
-
-    } else {
-      console.log("Aucune donnée de consommation trouvée.");
     }
 
     return wrapper;
   },
 
+  notificationReceived (notification) {
+    switch (notification) {
+      case "MODULE_DOM_CREATED":
+        this.sendSocketNotification("INIT", this.config);
+        break;
+    }
+  },
+
   socketNotificationReceived (notification, payload) {
-    if (notification === "CONSUMPTION_SCHEDULE") {
-      this.getData();
-      return;
-    }
-
-    if (notification === "CONSUMPTION_DATA") {
-      console.log("Données brutes reçues de l'API dans le module : ", payload);
-
-      const payloadArray = Array.isArray(payload) ? payload : [payload];
-
-      payloadArray.forEach((item) => {
-        if (item.start && item.end && item.interval_reading) {
-          const { start, interval_reading } = item;
-          const year = start.split("-")[0];
-
-          if (!this.consumptionData[year]) {
-            this.consumptionData[year] = [];
-          }
-
-          interval_reading.forEach((reading) => {
-            const day = parseInt(reading.date.split("-")[2]);
-            const month = parseInt(reading.date.split("-")[1]) - 1;
-            const value = parseFloat(reading.value);
-
-            const isDuplicate = this.consumptionData[year].some(
-              (entry) => entry.day === day && entry.month === month && entry.value === value
-            );
-
-            if (!isDuplicate) {
-              this.consumptionData[year].push({ day, month, value });
-            }
-          });
-        } else {
-          console.error("Format inattendu des données :", item);
-        }
-      });
-
-      this.updateDom();
-    }
-  },
-
-  calculateDates (yearOffset = 0) {
-    const today = new Date();
-    today.setFullYear(today.getFullYear() - yearOffset);
-    const endDate = today.toISOString().split("T")[0];
-
-    let startDate;
-
-    switch (this.config.periode) {
-      case 1:
-        startDate = new Date(today);
-        startDate.setDate(today.getDate() - 1);
+    switch (notification) {
+      case "ERROR":
+        console.error("[LINKY]", payload);
+        this.sendNotification("SHOW_ALERT", {
+          type: "notification",
+          title: "MMM-Linky",
+          message: payload,
+          timer: 10000
+        });
         break;
-      case 2:
-        startDate = new Date(today);
-        startDate.setDate(today.getDate() - 3);
+      case "CONSUMPTION_DATA":
+        this.consumptionData = payload;
+        this.updateDom(1000);
         break;
-      case 3:
-        startDate = new Date(today);
-        startDate.setDate(today.getDate() - 7);
-        break;
-      default:
-        Log.error("MMM-Linky: periode invalide.");
-        return null;
-    }
-
-    return { startDate: startDate.toISOString().split("T")[0], endDate };
-  },
-
-  loadChartJs (callback) {
-    if (typeof Chart === "undefined") {
-      const script = document.createElement("script");
-      script.src = "https://cdn.jsdelivr.net/npm/chart.js";
-      script.onload = callback;
-      document.head.appendChild(script);
-    } else {
-      callback();
-    }
-  },
-
-  ensureChartJsLoaded (callback) {
-    if (!this.ChartJsLoaded) {
-      this.loadChartJs(() => {
-        const script = document.createElement("script");
-        script.src = "https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels";
-        script.onload = () => {
-          this.ChartJsLoaded = true;
-          callback();
-        };
-        document.head.appendChild(script);
-      });
-    } else {
-      callback();
     }
   },
 
@@ -353,13 +247,11 @@ Module.register("MMM-Linky", {
 
   calculateTotalConsumption (year) {
     let total = 0;
-
     if (this.consumptionData[year]) {
       this.consumptionData[year].forEach((data) => {
         total += data.value;
       });
     }
-
     return total;
   }
 });
--- node_helper.js
+++ node_helper.js
@@ -1,65 +1,138 @@
 const NodeHelper = require("node_helper");
-const axios = require("axios");
-const schedule = require("node-schedule");
+
+var log = () => { /* do nothing */ };
 
 module.exports = NodeHelper.create({
 
   start () {
-    console.log("MMM-Linky node_helper démarré...");
-    this.scheduleDataFetch();
-  },
-
-
-  scheduleDataFetch () {
-    const randomHour = Math.floor(Math.random() * (12 - 10 + 1)) + 10;
-    const randomMinute = Math.floor(Math.random() * 60);
-    const cronExpression = `${randomMinute} ${randomHour} * * *`;
-    schedule.scheduleJob(cronExpression, () => {
-      console.log(`Exécution de la récupération des données à ${randomHour}:${randomMinute < 10 ? "0" : ""}${randomMinute}`);
-      this.sendnotificationschedule();
-    });
-
-    console.log(`Tâche planifiée pour ${randomHour}:${randomMinute < 10 ? "0" : ""}${randomMinute} UTC.`);
-  },
-
-
-  sendnotificationschedule () {
-    this.sendSocketNotification("CONSUMPTION_SCHEDULE");
+    this.Linky = null;
+    this.config = null;
+    this.dates = [];
+    this.consumptionData = {};
   },
 
   socketNotificationReceived (notification, payload) {
-
-    if (notification === "GET_CONSUMPTION_DATA") {
-      console.log("Notification GET_CONSUMPTION_DATA reçue, récupération des données...");
-      const { url, token, prm, startDate, endDate, period } = payload;
-      this.getConsumptionData(url, token, prm, startDate, endDate, period);
+    switch (notification) {
+      case "INIT":
+        this.config = payload;
+        this.initialize();
+        break;
     }
   },
 
-  getConsumptionData (url, token, prm, startDate, endDate) {
-    const endpoint = `${url}?prm=${prm}&start=${startDate}&end=${endDate}`;
-    const config = {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json"
+  // intialisation de MMM-Linky
+  async initialize () {
+    console.log(`[LINKY] MMM-Linky Version: ${require("./package.json").version} Revison: ${require("./package.json").rev}`);
+    if (this.config.debug) log = (...args) => { console.log("[LINKY]", ...args); };
+    log("Config:", this.config);
+    this.Dates = await this.getDates();
+    if (!this.Dates.length) return;
+    log("Dates:", this.Dates);
+    const { Session } = await this.loadLinky();
+    try {
+      this.Linky = new Session(this.config.token, this.config.prm);
+    } catch (error) {
+      console.error(`[LINKY] ${error}`);
+      this.sendSocketNotification("ERROR", error.message);
+      return;
+    }
+    this.scheduleDataFetch();
+  },
+
+  // Récupération planifié des données
+  scheduleDataFetch () {
+    this.getConsumptionData();
+    setInterval(() => {
+      this.getConsumptionData();
+    }, 1000 * 60 * 60);
+  },
+
+  // Récupération des données
+  async getConsumptionData () {
+    this.consumptionData = {};
+    await Promise.all(this.Dates.map(
+      async (date) => {
+        await this.sendConsumptionRequest(date).then((result) => {
+          if (result.start && result.end && result.interval_reading) {
+            const year = result.start.split("-")[0];
+            log(`-${year}- Données reçues de l'API :`, result);
+            this.consumptionData[year] = [];
+
+            result.interval_reading.forEach((reading) => {
+              const day = parseInt(reading.date.split("-")[2]);
+              const month = parseInt(reading.date.split("-")[1]);
+              const value = parseFloat(reading.value);
+
+              const isDuplicate = this.consumptionData[year].some(
+                (entry) => entry.day === day && entry.month === month && entry.value === value
+              );
+
+              if (!isDuplicate) {
+                this.consumptionData[year].push({ day, month, value });
+              }
+            });
+          } else {
+            console.error("Format inattendu des données :", result);
+          }
+        });
       }
-    };
+    ));
+    log("Final data:", this.consumptionData);
+    if (Object.keys(this.consumptionData).length) this.sendSocketNotification("CONSUMPTION_DATA", this.consumptionData);
+  },
 
-    console.log(`Requête API envoyée : ${endpoint}`);
-
-    axios
-      .get(endpoint, config)
-
-      .then((response) => {
-        console.log("Données reçues de l'API :", response.data);
-        const data = response.data;
-
-        this.sendSocketNotification("CONSUMPTION_DATA", data);
-      })
-
-      .catch((error) => {
-        console.error("Erreur lors de la récupération des données :", error.response ? error.response.data : error.message);
+  // Demande des datas selon l'API
+  sendConsumptionRequest (date) {
+    return new Promise((resolve) => {
+      this.Linky.getDailyConsumption(date.startDate, date.endDate).then((result) => {
+        resolve(result);
       });
-  }
+    });
+  },
 
+  // importation de la librairie linky (dynamic impor)
+  async loadLinky () {
+    const loaded = await import("linky");
+    return loaded;
+  },
+
+  // cacul des dates périodique
+  calculateDates (yearOffset = 0) {
+    const today = new Date();
+    today.setFullYear(today.getFullYear() - yearOffset);
+    const endDate = today.toISOString().split("T")[0];
+    const startDate = new Date(today);
+
+    switch (this.config.periode) {
+      case 1:
+        startDate.setDate(today.getDate() - 1);
+        break;
+      case 2:
+        startDate.setDate(today.getDate() - 3);
+        break;
+      case 3:
+        startDate.setDate(today.getDate() - 7);
+        break;
+      default:
+        console.error("[Linky] periode invalide.");
+        this.sendSocketNotification("ERROR", "periode invalide.");
+        return null;
+    }
+
+    return { startDate: startDate.toISOString().split("T")[0], endDate };
+  },
+
+  // Récupere les Dates pour l'intérogation de l'API
+  getDates () {
+    var Dates = [];
+    return new Promise((resolve) => {
+      var date = this.calculateDates();
+      if (date) Dates.push(date);
+      if (this.config.annee_n_minus_1 === 1) {
+        date = this.calculateDates(1);
+        if (date) Dates.push(date);
+      }
+      resolve(Dates);
+    });
+  }
 });
