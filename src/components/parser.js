bbec48d009b31127775325d1a52341cfebba8247
bugsounet <github@bugsounet.fr>
2025-03-02 16:48:13 +0100

    add LeapYear

--- src/components/parser.js
+++ src/components/parser.js
@@ -1,7 +1,9 @@
 const dayjs = require("dayjs");
 const isBetween = require("dayjs/plugin/isBetween");
+const isLeapYear = require("dayjs/plugin/isLeapYear");
 
 dayjs.extend(isBetween);
+dayjs.extend(isLeapYear);
 
 var log = () => { /* do nothing */ };
 
@@ -16,6 +18,7 @@ class PARSER {
     log("Démarrage...");
     var data = {};
     var added = 0;
+    var LeapYear = false;
 
     result.interval_reading.forEach((reading) => {
       const year = dayjs(reading.date).get("year");
@@ -25,7 +28,9 @@ class PARSER {
 
       if (this.config.annee_n_minus_1 === 1) {
         var current = dayjs().set("hour", 0).set("minute", 0).set("second", 0);
+        const currentIsLeapYear = current.isLeapYear();
         const currentYear = current.year();
+
         var testDate = current.subtract(1, "day");
         switch (this.config.periode) {
           case 1:
@@ -45,7 +50,24 @@ class PARSER {
           testDate = testDate.subtract(1, "year");
           current = current.subtract(1, "day").subtract(1, "year");
         }
+        const testDateIsLeapYear = testDate.isLeapYear();
+
         if (dayjs(reading.date).isBetween(testDate, current)) {
+          // LeapYear testing
+          if (currentIsLeapYear && dayjs(reading.date).month() === 1 && dayjs(reading.date).date() === 29) {
+            LeapYear = true;
+            log(`Année bissextile pour ${year}:`, { date: `${year - 1}-02-29`, value: 0 });
+            if (!data[year - 1]) data[year - 1] = [];
+            data[year - 1].push({ date: `${year - 1}-02-29`, value: 0 });
+            added++;
+          }
+          if (testDateIsLeapYear && dayjs(reading.date).month() === 1 && dayjs(reading.date).date() === 29) {
+            LeapYear = true;
+            log(`Année bissextile: ${year}:`, { date: `${year + 1}-02-29`, value: 0 });
+            if (!data[year + 1]) data[year + 1] = [];
+            data[year + 1].push({ date: `${year + 1}-02-29`, value: 0 });
+            added++;
+          }
           log(`Ajoute pour ${year}:`, { date: reading.date, value });
           data[year].push({ date: reading.date, value });
           added++;
@@ -56,6 +78,23 @@ class PARSER {
         added++;
       }
     });
+    if (LeapYear) {
+      for (const year in data) {
+        log(`Classements des dates pour ${year}...`);
+        data[year].sort((a, b) => {
+          if (a.date < b.date) {
+            return -1;
+          }
+          if (a.date > b.date) {
+            return 1;
+          }
+          return 0;
+        });
+        log(`Suppression des premières données pour ${year}:`, data[year][0]);
+        data[year].shift();
+        added--;
+      }
+    }
     log(`Terminé: ${added} dates trouvées.`);
     return data;
   }
